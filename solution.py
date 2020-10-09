94e8a30ab9f191f9e8b1dc72cad3c30e0b1f9310
jab9643 <72221111+jab9643@users.noreply.github.com>
2020-10-08 20:41:25 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -38,7 +38,7 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
 
     # Send RCPT TO command and print server response.
     # Fill in start
-    rcptTo = "RCPT TO:<name345@nyu.edu> \r\n"
+    rcptTo = "RCPT TO:<name345@nyu.edu>\r\n"
     clientSocket.send(rcptTo.encode())
     recv3 = clientSocket.recv(1024).decode()
     #print(recv3)
